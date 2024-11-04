d77ec1c0f7673182d3d0bb1c10ab7331281b07c7
amirali-cpu <ezpz.potatoaim@gmail.com>
2024-11-04 15:52:38 +0330

    commit3

---
+++ resources/views/branch1.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+<h1> group 1</h1>
+<ul>
+    <li>  your name</li>
+    <li>  your name</li>
+    <li>  your name</li>
+</ul>
+</body>
+</html>
